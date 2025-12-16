fb695927e4f05ccfe246b4d2874ae047acb30399
dbarbanakov <dbarbanakov@gmail.com>
2025-12-16 20:38:47 +0200

    data gets loaded from a json file

--- automatic_file_sorter/script.py
+++ automatic_file_sorter/script.py
@@ -3,18 +3,10 @@ from datetime import datetime
 import shutil
 import sys
 import time
+import json
 
 # Create a global variable for the main directory.
 main_dir = Path.cwd()
-
-
-def handle_test_dir():
-    # Moves files from test directory, if present, to the main directory, deletes it when done.
-    test_dir = main_dir / "test"
-    if test_dir.exists():
-        for file in test_dir.iterdir():
-            shutil.move(file, main_dir)
-        test_dir.rmdir()
 
 
 def get_dir(folder_name, parent_dir=main_dir):
@@ -23,19 +15,6 @@ def get_dir(folder_name, parent_dir=main_dir):
     if not dir.is_dir():
         dir.mkdir()
     return dir
-
-
-def rename_file(file, parent_dir):
-    # The format will look like this - script(i).py
-    # Where (i) is the increment, representing the next unique number.
-    i = 1
-    incremented_file = file.stem + "(" + str(i) + ")" + file.suffix
-
-    while (parent_dir / incremented_file).exists():
-        i += 1
-        incremented_file = f"{file.stem}({i}){file.suffix}"
-
-    return incremented_file
 
 
 def move_to_dir(dir_name, file):
@@ -51,21 +30,47 @@ def move_to_dir(dir_name, file):
         shutil.move(file, (dir / renamed_file))
 
 
-def main():
+def rename_file(file, parent_dir):
+    # The format will look like this - script(i).py
+    # Where (i) is the increment, representing the next unique number.
+    i = 1
+    incremented_file = file.stem + "(" + str(i) + ")" + file.suffix
+
+    while (parent_dir / incremented_file).exists():
+        i += 1
+        incremented_file = file.stem + "(" + str(i) + ")" + file.suffix
+
+    return incremented_file
+
+
+def handle_test_dir():
+    # Moves files from test directory, if present, to the main directory, deletes it when done.
+    test_dir = main_dir / "test"
+    if test_dir.exists():
+        for file in test_dir.iterdir():
+            shutil.move(file, main_dir)
+        test_dir.rmdir()
+
+
+def get_map():
+    with open("extention_to_dir_map.json", "r") as f:
+        return json.load(f)
+
+
+def main(map):
     # Creates the directory where files will be sorted in folders.
     sorted_files_dir = get_dir("sorted_files")
 
     for file in main_dir.iterdir():
         if file.is_file():
-            # If the file's name is the same as the script's name or "README.md" skip it.
-            if file.name in (sys.argv[0], "README.md"):
+            if file.name in (sys.argv[0], "README.md", "extention_to_dir_map.json"):
                 continue
 
             # Get the file extention of each file.
             file_extention = file.suffix
 
             # Get the destination's folder name mapped to the file's extention.
-            folder_name = files_to_dirs.get(file_extention)
+            folder_name = map.get(file_extention)
             # If the extention is not present, move it in another folder.
             if not folder_name:
                 move_to_dir("not_assigned", file)
@@ -83,19 +88,10 @@ def main():
 
 if __name__ == "__main__":
     handle_test_dir()
-
-    # A map of file extention to the destination folder.
-    files_to_dirs = {
-        ".py": "Python",
-        ".html": "Web",
-        ".jpeg": "Images",
-        ".png": "Images",
-        ".csv": "Data",
-        ".xlsx": "Data",
-    }
+    map = get_map()
 
     while True:
-        main()
+        main(map)
 
         now = datetime.now().isoformat(sep="@", timespec="seconds")
         time_sorted = (
